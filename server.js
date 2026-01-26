a24799c79a183ddd5ee293cb38adfdc2a74dd2d9
gabrielrodrigues-web <gabrielrodrigues@sesmat.com.br>
2026-01-26 16:33:22 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  console.log('User connected to secure TI line');
-  socket.on('mensagem', (msg) => {
-    io.emit('mensagem', msg);
+  console.log('Alguém entrou no chat GSO');
+  
+  socket.on('chat message', (msg) => {
+    // Aqui ele repassa o balão, a cor, o usuário e o texto
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Alguém saiu');
   });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log('SECURITY PROTOCOL ACTIVE ON PORT ' + PORT);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
