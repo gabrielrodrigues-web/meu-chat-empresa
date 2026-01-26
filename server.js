641af86ea9b38c80315c679f402efbe049757288
gabrielrodrigues-web <gabrielrodrigues@sesmat.com.br>
2026-01-26 15:34:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,24 +3,21 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// Faz o servidor entregar a página HTML
+// Isso faz o servidor ler o seu arquivo index.html
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Escuta quando alguém se conecta
 io.on('connection', (socket) => {
-    console.log('Alguém entrou no chat!');
-    
-    // Repassa a mensagem recebida para todo mundo
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
+  console.log('Alguém entrou no chat!');
+  
+  socket.on('mensagem', (msg) => {
+    io.emit('mensagem', msg); // Manda a mensagem para todos
+  });
 });
 
-// Substitua as linhas 21 a 24 por estas:
-const PORT = process.env.PORT || 3000;
-
+// Usa a porta que o Render exige
+const PORT = process.env.PORT || 10000;
 http.listen(PORT, () => {
-    console.log('CHAT ONLINE: Servidor rodando na porta ' + PORT);
+  console.log('CHAT ONLINE: Servidor rodando na porta ' + PORT);
 });
