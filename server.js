229d14c7619b274e16169c391b00fe0ca1885198
gabrielrodrigues-web <gabrielrodrigues@sesmat.com.br>
2026-01-27 08:49:04 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,24 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { maxHttpBufferSize: 1e7 });
 
+// Define que os arquivos estão na pasta principal
 app.use(express.static(__dirname));
+
+// FORÇA o envio do index.html ao abrir o site
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', (socket) => {
-    // Entra em um canal específico
     socket.on('join room', (room) => {
         socket.leaveAll();
         socket.join(room);
     });
 
     socket.on('chat message', (msg) => {
-        // Envia a mensagem apenas para quem está no mesmo canal
         io.to(msg.room).emit('chat message', msg);
     });
 });
 
 const PORT = process.env.PORT || 10000;
 http.listen(PORT, () => console.log('Servidor dual rodando na porta ' + PORT));
-
